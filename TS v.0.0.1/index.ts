0d8dd8e9617ff7326a546fcf2b3de2f1ad8a00d2
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2025-04-23 21:26:34 +0400

    TS Version Unpatched

--- TS v.0.0.1/index.ts
+++
@@ -1,9 +0,0 @@
-import adapters from './lib/adapter/adapters.js';
-import errStack from './lib/core/Error.js';
-import retry from './lib/core/retry.js';
-
-export {
-    adapters,
-    errStack,
-    retry,
-};
